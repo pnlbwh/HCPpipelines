997e0f0a68bc68797f6818de276a3cc7199630f7
Billah, Tashrif <tbillah@bwh.harvard.edu>
2021-07-27 14:16:36 -0400

    adjust w/space

--- DiffusionPreprocessing/scripts/run_eddy.sh
+++ DiffusionPreprocessing/scripts/run_eddy.sh
@@ -632,19 +632,19 @@ main() {
      
     # Redoing eddy without --repol flag to obtain not outlier replaced <=500 bshell
     if [ ! -z "${extra_eddy_args}" ]; then
-		for extra_eddy_arg in ${extra_eddy_args}; do
+	    for extra_eddy_arg in ${extra_eddy_args}; do
 
-			if [ $extra_eddy_arg = '--repol' ]; then
-                log_Msg "Redoing eddy without --repol flag to obtain not outlier replaced b<=500 shells"
-                eddy_command="${HCPPIPEDIR}/DiffusionPreprocessing/scripts/eddy_pnl_repol.py ${eddy_command}"
-                log_Msg "${eddy_command}"
-                ${eddy_command}
-                eddyReturnValue=$?
-                log_Msg "Completed with return value: ${eddyReturnValue}"
-		    fi
+		if [ $extra_eddy_arg = '--repol' ]; then
+                    log_Msg "Redoing eddy without --repol flag to obtain not outlier replaced b<=500 shells"
+                    eddy_command="${HCPPIPEDIR}/DiffusionPreprocessing/scripts/eddy_pnl_repol.py ${eddy_command}"
+                    log_Msg "${eddy_command}"
+                    ${eddy_command}
+                    eddyReturnValue=$?
+                    log_Msg "Completed with return value: ${eddyReturnValue}"
+		fi
 
-        done
-	fi
+            done
+    fi
 
     exit ${eddyReturnValue}
 
